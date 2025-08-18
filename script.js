5c9f11a9355991ad274b5a46ef3751c67fed8e00
ErickFerraz7 <0000112098418xsp@al.educacao.sp.gov.br>
2025-08-18 08:33:18 -0300

    V4.5

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded',function()){
+    const aumentaFonteBotao = document.getElementByIdI('aumentar-fonte');
+     const diminuirFonteBotao = document.getElementByIdI('diminuir-fonte');
+
+    let tamanhoAtualFonte = 1;
+
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonten += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
+
+    diminuirFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonten -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
+
+}//falta parenteses?
